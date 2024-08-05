577db33a83f749ada81a56cae98cb6acdbbc1fdc
Vladislav_14 <vladislav.kolos.y@gmail.com>
2024-08-05 16:53:46 +0300

    001 Implemented and configured connection to the database
        Configured configuration class "WebAppConfig" to connect to the database and use the "Transactional" annotation
    
        Implemented and configured logging to the console
    
        Implemented exception handling if they occur

---
+++ src/main/java/org/example/config/WebAppConfig.java
@@ -0,0 +1,27 @@
+package org.example.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class WebAppConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("org.example.model");
+        return entityManagerFactoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(entityManagerFactory);
+        return transactionManager;
+    }
+}
